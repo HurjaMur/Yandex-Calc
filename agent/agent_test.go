5228be3607e469232d37f0e41691d12707bd7e17
HurjaMur <153192124+HurjaMur@users.noreply.github.com>
2024-04-22 00:04:43 +0300

    Update agent_test.go
    add test

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -1,1 +1,45 @@
+package agent
+
+import (
+	"testing"
+)
+
+func TestProcessExpression_Success(t *testing.T) {
+	tests := []struct {
+		expression string
+		tim        int
+		expected   string
+	}{
+		{"2 + 2", 1, "4"},
+		{"10 - 5", 2, "5"},
+		{"3 * 4", 3, "12"},
+		{"12 / 3", 4, "4"},
+		{"2 + 3 * 4", 5, "14"}, // Проверка приоритета операций
+	}
+
+	for _, test := range tests {
+		result := ProcessExpression(test.expression, test.tim)
+		if result != test.expected {
+			t.Errorf("ProcessExpression(%q, %d) = %q, expected %q", test.expression, test.tim, result, test.expected)
+		}
+	}
+}
+
+func TestProcessExpression_Error(t *testing.T) {
+	tests := []struct {
+		expression string
+		tim        int
+		expected   string
+	}{
+		{"2 + a", 1, "Ошибка: неверный формат числа"},
+		{"10 / 0", 2, "Ошибка: деление на ноль"},
+	}
+
+	for _, test := range tests {
+		result := ProcessExpression(test.expression, test.tim)
+		if result != test.expected {
+			t.Errorf("ProcessExpression(%q, %d) = %q, expected %q", test.expression, test.tim, result, test.expected)
+		}
+	}
+}
 
