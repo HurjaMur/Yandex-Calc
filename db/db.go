e1c2b1b4ea97dfcdfde52c00090eded75c92392c
HurjaMur <153192124+HurjaMur@users.noreply.github.com>
2024-04-22 00:15:16 +0300

    Update db.go

--- db/db.go
+++ db/db.go
@@ -8,29 +8,34 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Функция для создания и инициализации базы данных
 func CreateDB() *sql.DB {
-	ctx := context.TODO()
-	database, err := sql.Open("sqlite3", "db/users.db")
-	if err != nil {
-		log.Fatal(err)
-	}
+ ctx := context.TODO() // Создание контекста
 
-	err = database.PingContext(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+ // Открытие базы данных sqlite3
+ database, err := sql.Open("sqlite3", "db/users.db")
+ if err != nil {
+  log.Fatal(err) // Вывод фатальной ошибки, если не удалось открыть базу данных
+ }
 
-	_, err = database.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS users (login TEXT, password TEXT expression TEXT, result INTEGER)")
-	if err != nil {
-		log.Fatal(err)
-	}
+ // Проверка соединения с базой данных
+ err = database.PingContext(ctx)
+ if err != nil {
+  log.Fatal(err) // Вывод фатальной ошибки, если не удалось подключиться к базе данных
+ }
 
-	_, err = database.ExecContext(ctx, "INSERT INTO users (login, password, expression, result) VALUES (?, ?, ?)")
+ // Создание таблицы "users", если она не существует
+ _, err = database.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS users (login TEXT, password TEXT, expression TEXT, result INTEGER)")
+ if err != nil {
+  log.Fatal(err) // Вывод фатальной ошибки, если не удалось создать таблицу
+ } 
+	
+  _, err = database.ExecContext(ctx, "INSERT INTO users (login, password, expression, result) VALUES (?, ?, ?)")
+	
+  _, err = database.Query("SELECT login, password expression, result FROM users")
 
-	_, err = database.Query("SELECT login, password expression, result FROM users")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	return database
+  if err != nil {
+    log.Fatal(err)
+  }
+ return database // Возвращение объекта базы данных
 }
